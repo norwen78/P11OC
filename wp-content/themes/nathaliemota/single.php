ba8b3b0f46f4fe1e995997d8499821b058779629
Norwen <norwen78710@hotmail.fr>
2024-01-29 15:19:56 +0100

    minia navigation single

--- wp-content/themes/nathaliemota/single.php
+++ wp-content/themes/nathaliemota/single.php
@@ -97,28 +97,33 @@ get_header(); // Ajoutez le code d'en-tête standard de WordPress
         </div>
 
         <div class="photo-navigation">
-                <?php
-                // Récupère les photos précédente et suivante//
-                $prev_post = get_previous_post();
-                $next_post = get_next_post();
-                ?>
+            <?php
+            // Récupère les photos précédente et suivante
+            $prev_post = get_previous_post();
+            $next_post = get_next_post();
+            ?>
+            
+                <div class="navigation_thumbnail">
+                <?php echo get_the_post_thumbnail($next_post->ID, 'thumbnail', array('class' => 'mini-thumbnail')); ?>
+                </div>
 
-                <?php if (!empty($prev_post)) : ?>
-                    <!-- Lien pour la photo précédente -->
-                    <a href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>" class="arrow-thumbnail custom-prev-link">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/arrow-left.png" class="arrow" data-direction="previous" alt="Previous">
-                        <?php echo get_the_post_thumbnail($prev_post->ID, 'thumbnail', array('class' => 'mini-thumbnail')); ?>
-                    </a>
-                <?php endif; ?>
+            <div class="navigation_arrow">
+            <?php if (!empty($prev_post)) : ?>
+                <!-- Lien pour la photo précédente -->
+                <a href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>" class="arrow-thumbnail custom-prev-link">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/arrow-left.png" class="arrow" data-direction="previous" alt="Previous">
+                </a>
+            <?php endif; ?>
 
-                <?php if (!empty($next_post)) : ?>
-                    <!-- Lien pour la photo suivante -->
-                    <a href="<?php echo esc_url(get_permalink($next_post->ID)); ?>" class="arrow-thumbnail custom-next-link">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/arrow-right.png" class="arrow" data-direction="next" alt="Next">
-                        <?php echo get_the_post_thumbnail($next_post->ID, 'thumbnail', array('class' => 'mini-thumbnail')); ?>
-                    </a>
-                <?php endif; ?>
+            <?php if (!empty($next_post)) : ?>
+                <!-- Lien pour la photo suivante -->
+                <a href="<?php echo esc_url(get_permalink($next_post->ID)); ?>" class="arrow-thumbnail custom-next-link">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/arrow-right.png" class="arrow" data-direction="next" alt="Next"> 
+                </a>
+            <?php endif; ?>
             </div>
+        </div>
+
         <?php endwhile; // Fin de la boucle WordPress ?>
         </div>
 
